5709a70f089c14fd1a05c04b5013ed7e3aca7060
Jacob Smith <jacob.regsmith@gmail.com>
2024-05-27 10:52:29 -0400

    clean up some animations and object borders.

--- Scenes/scripts/dizzy.gd
+++ Scenes/scripts/dizzy.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-
 const SPEED = 200
-const JUMP_VELOCITY = -675.0
+const JUMP_VELOCITY = -575.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -24,7 +23,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
